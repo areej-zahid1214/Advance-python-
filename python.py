97f6037c3b9b0c3b08726698b6ca21c0436e8df4
Areej Zahid <syedaareejjabbar@gmail.com>
2026-08-19 17:41:01 +0500

    Pratice with consistency

--- python.py
+++ python.py
@@ -1,7 +1,45 @@
-# #WAP to input user First name's and print it length
+# WAP to input user First name's and print it length
 # name=input("Enter you first name")
 # print(len(name))
 
-#Wap to find the accurance of $ in a string
-str=("My name is Areej Zahid")
-print(str.find("$"))
+# #Wap to find the accurance of $ in a string
+# str=("My name is Areej Zahid")
+# print(str.find("$"))
+
+# marks=int(input("Enter your marks"))
+# if(marks>=90):
+#     print("A grade")
+# elif(marks>=80):
+#     print(" B grade")
+# elif(marks>=70):
+#     print(" c grade")
+# else:
+#     print("fail")
+    
+    
+    
+# num=int(input("Enter the no"))
+# if (num/2==0):
+#      print("The no is even")
+# else:
+#     print("your no is odd")
+
+# a=int(input("Enter the 1st no"))
+# b=int(input("Enter the 2nd no"))
+# c=int(input("Enter the 3rd no"))
+# d=int(input("Enter the 3rd no"))
+# if( a>=b and a>=c and a>=d):
+#     print("A is greater")
+# elif( b>=c and b>=d and b>=a):
+#     print("b is greater")
+# elif( c>=d and c>=a and c>=b):
+#     print("C is graeter")   
+# else:
+#  print("d is greater")
+   
+   
+num=int(input("Enter the no"))
+if (num%7==0):
+    print("Yours no is multiple of Seven")
+else:
+   print("its not a multiple of seven")
