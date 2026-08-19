fc3303797794fa1e40cb79405dc734a071201e78
Areej Zahid <syedaareejjabbar@gmail.com>
2026-08-19 16:30:10 +0500

    Practice

--- python.py
+++ python.py
@@ -1,22 +1,7 @@
-# Write the sum of Two no
-# num1=int(input("Enter the no"))
-# num2=int(input("Enter the no"))
-# print("sum of 2 no is", num1+num2)
+# #WAP to input user First name's and print it length
+# name=input("Enter you first name")
+# print(len(name))
 
-
-
- #Area of a rectangular       
-# Area=int(input("Enter the one side or Rec Area"))
-# print("Total Area is",Area**2)
-
-# WAP a program of input two floating  no and print their average
-# num1=float(input("Enter the no"))
-# num2=float(input("Enter the no"))
-# print("Average of two no",(num1+num2)/2)
-
-
-#WAP to input two int no a and b 
-#print True if A is Greater than or equal to b if not print false
-a=int(input("Enter the no"))
-b=int(input("Enter the no"))
-print(a>=b)
+#Wap to find the accurance of $ in a string
+str=("My name is Areej Zahid")
+print(str.find("$"))
