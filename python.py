ebb167e54569fce5acf9030e434ea8870e9652a0
Areej Zahid <syedaareejjabbar@gmail.com>
2026-08-20 15:37:27 +0500

    I have learned a lot of things

--- python.py
+++ python.py
@@ -1,7 +1,26 @@
-# list=["C","D","A","A","B","B","A"]
-# print(list.count("A"))
+# dic={
+#     "table":["A piece of Furniture","List of fact and figure"],
+#     "Cat": "A small animal"
+# }
+# print(dic)
 
 
-list=["C","D","A","A","B","B","A"]
-list.sort()
-print(list)
+
+# set={
+#     "python","java","C++","python","javascript","JAVA","python","java","C++","C"
+#     }
+# print(len(set))
+
+sub={}
+a=input("Enter your  1st subject")
+a1= input("Enter the marks")
+b=input("Enter your   2nd subject")
+b1= input("Enter the marks")
+c=input("Enter your  3rd subject")
+c1= input("Enter the marks")
+sub.update({
+a:a1,
+b:b1,
+c:c1
+})
+print(sub)
