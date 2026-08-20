8d01dfa7d8e82a6656dd5b2405319cb4cb17747f
Areej Zahid <syedaareejjabbar@gmail.com>
2026-08-20 11:34:50 +0500

    A Python script that calculates ticket prices based on quantity, age discounts, promo codes, and weekend surcharges.

--- python.py
+++ python.py
@@ -1,45 +1,20 @@
-# WAP to input user First name's and print it length
-# name=input("Enter you first name")
-# print(len(name))
-
-# #Wap to find the accurance of $ in a string
-# str=("My name is Areej Zahid")
-# print(str.find("$"))
-
-# marks=int(input("Enter your marks"))
-# if(marks>=90):
-#     print("A grade")
-# elif(marks>=80):
-#     print(" B grade")
-# elif(marks>=70):
-#     print(" c grade")
-# else:
-#     print("fail")
-    
-    
-    
-# num=int(input("Enter the no"))
-# if (num/2==0):
-#      print("The no is even")
-# else:
-#     print("your no is odd")
-
-# a=int(input("Enter the 1st no"))
-# b=int(input("Enter the 2nd no"))
-# c=int(input("Enter the 3rd no"))
-# d=int(input("Enter the 3rd no"))
-# if( a>=b and a>=c and a>=d):
-#     print("A is greater")
-# elif( b>=c and b>=d and b>=a):
-#     print("b is greater")
-# elif( c>=d and c>=a and c>=b):
-#     print("C is graeter")   
-# else:
-#  print("d is greater")
-   
-   
-num=int(input("Enter the no"))
-if (num%7==0):
-    print("Yours no is multiple of Seven")
+User=(input("Enter your name"  ))
+age=int(input("Enter your age"  ))
+day=(input("Whats the day" )).strip().lower()
+promo_code="Discount20"
+ticket_Quantity=int(input("Enter the Quantity of Tickets what you Want"  ))
+base_price=800.0
+total_Price=ticket_Quantity*base_price
+if (age<=12 or age>=60):
+    final_price=total_Price-(total_Price/2)
+    print("50% Discount is",final_price)
+elif (User==promo_code):
+    final_price=total_Price-(total_Price*0.3)
+    print('60% Discount is',final_price) 
+elif (day=="saturday" or day=="sunday"):
+    final_price=total_Price+(100*ticket_Quantity)
+    print("charges of Ticket is Extra Due to week days" ,final_price)
 else:
-   print("its not a multiple of seven")
+    print("Your Ticket price is",total_Price)
+    
+     
